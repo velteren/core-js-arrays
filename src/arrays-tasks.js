a1d4409056046f5de9361b565d109c5ba15a877b
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-03 16:06:34 +0300

    feat: add getIntervalArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,10 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return new Array(end - start + 1)
+    .fill(start)
+    .map((item, index) => item + index);
 }
 
 /**
