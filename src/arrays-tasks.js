da9df0d705b3659f4dafd504f3e0b40bcbebe597
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 22:27:44 +0300

    feat: add generateOdds

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -346,8 +346,9 @@ function createChunks(/* arr, chunkSize */) {
  *    generateOdds(2) => [ 1, 3 ]
  *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
  */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  const arr = new Array(len).fill(0);
+  return arr.map((item, index) => 2 * index + 1);
 }
 
 /**
