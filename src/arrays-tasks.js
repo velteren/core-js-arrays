a4284d64a1833afc976a23a579de99a1c75e2e32
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 21:08:37 +0300

    feat: implement findAllOccurrences

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -76,8 +76,8 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.filter((i) => i === item).length;
 }
 
 /**
