c9d577afb9e258efd12ad1a8ff4bc84c82b497b2
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 21:19:43 +0300

    feat: add getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -124,8 +124,10 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) return 0;
+  const sum = arr.reduce((acc, curr) => acc + curr);
+  return +(sum / arr.length).toFixed(2);
 }
 
 /**
