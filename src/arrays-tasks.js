cabfc0171fc59cbd1cfdbdb2f12d5ba367752ba1
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 22:22:59 +0300

    feat: add calculateBalance

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -314,8 +314,8 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.map((item) => item[0] - item[1]).reduce((sum, curr) => sum + curr);
 }
 
 /**
