f592ff318e8cb409ea314c58fb6d73c892aaf8a4
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 23:58:20 +0300

    feat: add shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -522,8 +522,10 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const tmpArr = arr.slice(-n);
+  arr.unshift(...tmpArr);
+  return arr.slice(0, arr.length - tmpArr.length);
 }
 
 /**
