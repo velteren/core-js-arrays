f59c7a38918ed1a7a54399290e46d44227a147fb
Constantine Saveliev <senior.saveliev2012@yandex.ru>
2024-12-04 23:00:57 +0300

    feat: add getIndicesOfOddNumbers

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -417,8 +417,10 @@ function getIdentityMatrix(n) {
  *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
  *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
  */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return Array.from(numbers.entries())
+    .filter((item) => item[1] % 2 !== 0)
+    .map((item) => item[0]);
 }
 
 /**
